d3449bc4e19c1761707eafbb37e18dedbbb72310
Kitui Kelly <kituikelly@gmail.com>
2024-05-26 00:31:01 +0300

    completes lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,23 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name="", last_name="", knowledge=[]):
+        super().__init__(first_name, last_name)
+        self.knowledge= [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
+   # This function selects a random piece of knowledge from the "knowledge" attribute and returns it
     def teach(self):
-        pass
+        # Generate a random index to select a random piece of knowledge
+        random_index = random.randint(0, len(self.knowledge)-1)
+        
+        # Return the random knowledge
+        return self.knowledge[random_index]
